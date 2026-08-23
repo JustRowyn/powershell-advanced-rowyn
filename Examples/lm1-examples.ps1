8af0e8ef24570abbb0fca45a306c8459801e9675
JustRowyn <rowynrodenbeck66@gmail.com>
2026-08-23 19:33:41 +0000

    Added Examples folder and lm1-examples.ps1

---
+++ Examples/lm1-examples.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+    Shows Scripting Best Practices
+
+    .DESCRIPTION
+    This is the longer description
+
+.PARAMETER ResourceGroupName
+
+.EXAMPLE
+    .\lm1-examples.ps1 "Rg-test"
+
+#>
+
+
+#Scripting Best Practices
+
+# Indentation: Use consistent indentation (e.g., 4 spaces) to improve readability.
+<#
+if ($true) {
+    Write-Output "This is an example of proper indentation."
+}
+    #>
+
+#Poor Vs Good Script
+#Poor Script
+<#$a = Read-Host "Name"
+
+New-AzResourceGroup -Name $a -Location "Central US"
+#>
+
+# Improved Script
+param (
+    [Parameter(Mandatory=$true)]
+    [string][ValidateLength(1,10)]$ResourceGroupName
+)
+
+New-AzResourceGroup `
+    -Name $ResourceGroupName `
+    -Location "Central US"
+
+# for loop example
+for ($i=0; $i -lt $ResourceGroupName.count; $i++) {
+
+}
